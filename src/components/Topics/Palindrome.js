f04be70752cea8301ad6721cd5defa2415d48666
SwiftVapor <jonathongthayne@gmail.com>
2020-09-16 23:51:54 -0600

    Made Palindrome work 100% of the time (i think)

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,40 @@
 import React, {Component} from 'react';
 
+
+
 export default class Palindrome extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+    handleChange(val){
+        this.setState({userInput:val});
+    }
+    isItPali = (userInput) =>{
+        let noSpaces = userInput.split(" ").join("");
+        let broken = noSpaces.split('');
+        for(let i = 0; i < broken.length; i++){
+            console.log (broken[i], i , broken[broken.length-[i+1]])
+            if (broken[i] !== broken[broken.length-[i+1]] ){
+                this.setState({ palindrome: 'false' });
+            }
+            else{
+                this.setState({ palindrome: 'true' });
+            }
+        }
+}
     render() {
         return (
-            <p> Palindrome</p>
+            <div className='puzzleBox filterStringPB'>
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(event) => this.handleChange(event.target.value)}/>
+                <button className="confirmationButton" onClick={ () => this.isItPali(this.state.userInput)} >Check</button>
+                <span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+            </div>
+
         )
     }
 
